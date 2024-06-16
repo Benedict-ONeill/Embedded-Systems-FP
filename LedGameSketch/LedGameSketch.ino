b8436f8d10bb3564788e50294dc2325cef45785a
Benedict <benedictoneill@outlook.com>
2024-06-17 09:22:13 +1200

    Imported libraries

--- LedGameSketch/LedGameSketch.ino
+++ LedGameSketch/LedGameSketch.ino
@@ -1,11 +1,18 @@
+#include <Keypad.h>
+#include <Key.h>
+
+const byte ROWS = 4;
+const byte COLUMNS = 4;
+
 void setup() {
   Serial.begin(9600);
+
 
   int gameValues[16];    //each integer in this array determines how many LEDs in each section of the matrix will be lit
   for (int x = 0; x <= 15; x++)
   {
     gameValues[x] = random(1, 5);   //determine the starting arrangement of the game.
-    Serial.print(gameValues[x]);
+    Serial.println(gameValues[x]);
   }
 }
 
