f299cc4d814a83832b5237d549fd604701cc74f6
Benedict <benedictoneill@outlook.com>
2024-06-17 08:55:16 +1200

    New sketch with initilization values

---
+++ LedGameSketch/LedGameSketch.ino
@@ -0,0 +1,15 @@
+void setup() {
+  Serial.begin(9600);
+
+  int gameValues[16];    //each integer in this array determines how many LEDs in each section of the matrix will be lit
+  for (int x = 0; x <= 15; x++)
+  {
+    gameValues[x] = random(1, 5);   //determine the starting arrangement of the game.
+    Serial.print(gameValues[x]);
+  }
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+  delay(1000);
+}
